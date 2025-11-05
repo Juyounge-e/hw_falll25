5f4b1629c883f8a60abdeffc5c2592c2b75b7fdb
Juyounge-e <ghn09081@gmail.com>
2025-11-05 20:49:18 +0900

    [todo2] 전문가 정책의 정답 행동 저장

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -141,8 +141,9 @@ def run_training_loop(params):
                 # TODO: relabel collected obsevations (from our policy) with labels from expert policy
                 # HINT: query the policy (using the get_action function) with paths[i]["observation"]
                 # and replace paths[i]["action"] with these expert labels
-                paths = TODO
-
+                for i in range(len(paths)):
+                    paths[i]['action'] = expert_policy.get_action(paths[i]['observation'])
+                    
         total_envsteps += envsteps_this_batch
         # add collected data to replay buffer
         replay_buffer.add_rollouts(paths)
